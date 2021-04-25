66c7d37b9741c975ee05dfa710ae1fd5caeb29e7
josh1506 <51785959+josh1506@users.noreply.github.com>
2021-04-25 17:08:42 +0800

    Add Landing and About page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Landing from './pages/Landing';
+import About from './pages/About'
+
+import './assets/styles/main.css'
 
 function App(props) {
     return (
-        <div>
-            
+        <div className='d-flex' style={{width: '80vw'}}>
+            {/* <Landing /> */}
+            <About />
         </div>
     );
 }
