a828ec84135213661dfc2e34ef0fee5530d2d173
josh1506 <51785959+josh1506@users.noreply.github.com>
2021-04-25 17:09:54 +0800

    Add arrow for next page

---
+++ src/components/common/RightArrow.jsx
@@ -0,0 +1,92 @@
+import React, { useState } from 'react'
+import { motion } from 'framer-motion'
+
+import '../assets/styles/common/arrow.css'
+
+const arrowTop = {
+    initial: {
+        opacity: 0,
+        rotate: -360
+    },
+    visible: {
+        opacity: 1,
+        rotate: 30, 
+        y: -12,
+        transition: {
+            duration: 0.3
+        }
+    },
+    onHover: {
+        rotate: 50, 
+        y: -18, 
+        x: 5
+    },
+    onClick: {
+        rotate: [65, 0, 0],  
+        y: [-22, 0, 0], 
+        x: [0, 0, 90], 
+        transition: {
+            duration: 0.2
+        }
+    }
+}
+
+const arrowBottom = {
+    initial: {
+        rotate: 360
+    },
+    visible: {
+        rotate: -30, 
+        y: 12,
+        transition: {
+            duration: 0.3
+        }
+    },
+    onHover: {
+        rotate: -50, 
+        y: 18, 
+        x: 5
+    },
+    onClick: {
+        rotate: [-65, 0, 0], 
+        y: [22, 0, 0], 
+        x:[0, 0, 90], 
+        transition: {
+            duration: 0.2
+        }
+    }
+}
+
+const RightArrow = (props) => {
+    const [btnRightHover, setBtnRightHover] = useState(false)
+    const [btnRightClick, setBtnRightClick] = useState(false)
+
+    return (
+        <div 
+            className='arrow-container' 
+            style={{right: 0, borderLeft: '2px salmon solid'}}
+            onMouseEnter={() => setBtnRightHover(true)} 
+            onMouseLeave={() => setBtnRightHover(false)}
+            onClick={() => setBtnRightClick(true)}
+        >
+            <div>
+                <motion.div
+                    variants={arrowTop}
+                    initial='hidden'
+                    animate={btnRightClick ? 'onClick' : btnRightHover? 'onHover' : 'visible'}
+                    className='arrow'
+                    style={{right: 0}}
+                />
+                <motion.div
+                    variants={arrowBottom}
+                    initial='hidden'
+                    animate={btnRightClick ? 'onClick' : btnRightHover? 'onHover' : 'visible'}
+                    className='arrow'
+                    style={{right: 0}}
+                />
+            </div>
+        </div>
+    )
+}
+
+export default RightArrow
