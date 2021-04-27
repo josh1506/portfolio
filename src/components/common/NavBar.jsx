04056ebe2e22a54d565d9bbbc416f519b5dcf6cf
josh1506 <51785959+josh1506@users.noreply.github.com>
2021-04-27 09:27:14 +0800

    Create NavBar.jsx

---
+++ src/components/common/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import '../assets/styles/common/navbar.css'
+
+const NavBar = () => {
+    return (
+        <nav>
+            <div>
+                <Link to='/' className='nav-link'>Landing</Link>
+                <Link to='/about' className='nav-link'>About</Link>
+                <Link to='/projects' className='nav-link'>Projects</Link>
+                <Link to='/contact' className='nav-link'>Contact</Link>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
