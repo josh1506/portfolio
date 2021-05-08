9add3bbf29c7ae8e88ebc39983bda3fa7640da0e
Joshua Michael Jabor <51785959+josh1506@users.noreply.github.com>
2021-05-08 21:13:11 +0800

    Update NavBar.jsx

--- src/components/common/NavBar.jsx
+++ src/components/common/NavBar.jsx
@@ -1,19 +1,27 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
-import '../assets/styles/common/navbar.css'
+import "../assets/styles/common/navbar.css";
 
 const NavBar = () => {
     return (
         <nav>
             <div>
-                <Link to='/' className='nav-link'>Landing</Link>
-                <Link to='/about' className='nav-link'>About</Link>
-                <Link to='/projects' className='nav-link'>Projects</Link>
-                <Link to='/contact' className='nav-link'>Contact</Link>
+                <Link to="/" className="nav-link">
+                    Landing
+                </Link>
+                <Link to="/about" className="nav-link">
+                    About
+                </Link>
+                <Link to="/projects" className="nav-link">
+                    Projects
+                </Link>
+                <Link to="/contact" className="nav-link">
+                    Contact
+                </Link>
             </div>
         </nav>
-    )
-}
+    );
+};
 
-export default NavBar
+export default NavBar;
