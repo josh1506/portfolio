5c27fd3d71c7e6618f8a901c6fe0c83f0b0ddc47
Joshua Michael Jabor <51785959+josh1506@users.noreply.github.com>
2021-05-09 22:20:19 +0800

    Update Projects.jsx

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -214,13 +214,6 @@ const Projects = (props) => {
                             onMouseLeave={() => handleMouseLeave(project)}
                             key={project.id}
                             variants={animateProject}
-                            drag
-                            dragConstraints={{
-                                left: 0,
-                                right: 0,
-                                top: 0,
-                                bottom: 0,
-                            }}
                         >
                             <a
                                 href={project.link}
